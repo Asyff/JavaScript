8fa5e24d6647fc9ad3ea0511671a3527119d5b8c
Asyff <astraltrinity84@gmail.com>
2024-08-13 22:03:48 +0545

    First commit

---
+++ Basic/FirstProject/index.js
@@ -0,0 +1,8 @@
+console.log(`Hello`);
+console.log(`Living is fun`);
+
+
+window.alert(`This is an alert!!!`);
+
+document.getElementById("hello").textContent=`Hello`;
+document.getElementById("paragraph").textContent=`I hate paragraphs`
